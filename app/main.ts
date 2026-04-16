e3ca2f902b4d44117dd2ac66d398ff342caf775b
amanlal <alal09635@gmail.com>
2026-04-16 01:03:41 -0700

    implemented cat-file command where blob is read decompressed and separated from content and header to output content

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,5 @@
 import * as fs from "fs";
+import * as zlib from "zlib";
 
 const args = process.argv.slice(2);
 const command = args[0];
@@ -14,6 +15,28 @@ switch (command) {
     fs.mkdirSync(".git/refs", { recursive: true });
     fs.writeFileSync(".git/HEAD", "ref: refs/heads/main\n");
     console.log("Initialized git directory");
+    break;
+  
+  case "cat-file":
+    const directory = args[2].slice(0,2);
+    const fileName =  args[2].slice(2);
+    
+    try {
+      // Read blob 
+      const content = fs.readFileSync(`.git/objects/${directory}/${fileName}`);
+
+      // Decompress
+      // Use .unzipSync when unsure whether header is zlib(buffer) or gzlib(.gz)
+      const decompressContent = zlib.unzipSync(content); // we could've used .inflateSync since we are dealing with zlib(buffer)
+
+      // Extract content
+      // ignore header(blob <size>\0)
+      const realContent = decompressContent.toString().split("\0")[1];
+      console.log(realContent);
+    } catch (err) {
+      console.error("Error reading and decompressing blob");
+    }
+
     break;
   default:
     throw new Error(`Unknown command ${command}`);
