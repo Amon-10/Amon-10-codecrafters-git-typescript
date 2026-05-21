45d88feb7e1fbc9df3a57fa6de430187e2da28b2
amanlal <alal09635@gmail.com>
2026-05-21 16:43:42 -0700

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -141,8 +141,8 @@ switch (command) {
       // convert sha raw bytes to hex string
       let hexString = Buffer.from(byte_sha).toString('hex'); // not required when codecafters submit
 
-      console.log(`${mode} ${hexString} ${objectFileName}`);
-      //console.log(`${objectFileName}`);
+      //console.log(`${mode} ${hexString} ${objectFileName}`);
+      console.log(`${objectFileName}`);
     }
 
     break;
